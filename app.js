449654950bba74c5806c3ae9e78e9c6d9bc60aa6
PrasiddhaKhadka <prasiddhak77@gmail.com>
2025-11-14 11:43:22 +0545

    Add: Backend Settingup

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+const mongoose = require("mongoose");
 
+const notFoundMiddleWare = require("./middlewares/not_found");
+
+//PORT NUMBER
+const PORT_NUMBER = 8000;
+
+// MIDDLE WARES
+app.use(express.json());
+
+// ROUTES
+
+
+// SERVER STARTING 
+app.listen(PORT_NUMBER, () => {
+    console.log(`Server started at ${PORT_NUMBER}`);
+})
