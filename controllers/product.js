74116af83d32e092dc41e205b3330686504ddef9
PrasiddhaKhadka <prasiddhak77@gmail.com>
2025-11-14 15:12:16 +0545

    Add: Controller and populate

--- controllers/product.js
+++ controllers/product.js
@@ -1,12 +1,30 @@
+const Product = require('../models/product')
+
 
 const getAllProducts= async(req, res) => {
+   const {company, name, featured} = req.query
+   const query0bject={}
+
+   if(featured){
+    query0bject.featured = featured === 'true' ? true : false
+   }
+
+   query_param = req.query
+   getProduct = await Product.find(query0bject);
     res.status(200).json({
-        message:'All Products'
+           message: "Success",
+           products: getProduct,
+           nbHits: getProduct.length       
     })
+  
+   
 }
-const getAllProductsStatic = (req, res) => {
+const getAllProductsStatic = async(req, res) => {
+    allProducts = await Product.find({});
     res.status(200).json({
-        message: "All Static Products"
+        message: "All Static Products",
+        products: allProducts,
+        nbHits: allProducts.length
     })
 }
 
